fed275a870dfd87fe089ecdaeda66d518781d563
Svar07 <svar-07@mail.ru>
2025-02-20 23:53:36 +0500

    Controller + record + BD + UnitTest

--- src/test/java/org/example/addressbook/AddressBookApplicationTests.java
+++ src/test/java/org/example/addressbook/AddressBookApplicationTests.java
@@ -15,44 +15,47 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@SpringBootTest  // Аннотация @SpringBootTest указывает, что это интеграционный тест, который загружает полный контекст Spring Boot.
+@AutoConfigureMockMvc // Аннотация @AutoConfigureMockMvc настраивает MockMvc для тестирования веб-слоя.
 class AddressBookApplicationTests {
-	@Autowired
+	@Autowired // Внедрение репозитория для работы с базой данных.
 	AddressBookRepository repository;
 
-	@Autowired
+	@Autowired // Внедрение WebTestClient для выполнения HTTP-запросов и проверки ответов.
 	WebTestClient webTestClient;
 
-	@Test
+	@Test  // Тест для проверки загрузки контекста Spring Boot.
 	void contextLoads(){
-		assertThat(webTestClient).isNotNull();
+		assertThat(webTestClient).isNotNull(); // Проверяем, что WebTestClient был успешно создан и не равен null.
 	}
-	@Test
+	@Test // Тест для проверки метода получения всех записей из адресной книги.
 	void testFindAll(){
-		webTestClient.get()
+		webTestClient.get() // Выполняем GET-запрос к эндпоинту /api/v1/addressbookS.
 				.uri("/api/v1/addressbookS")
-				.exchange()
-				.expectStatus().isOk()
-				.expectBody() // указание "[]" проверяет пустая ли база данных
-				.json("""
+				.exchange()// Отправляем запрос.
+				.expectStatus().isOk()// Ожидаем статус ответа 200 OK.
+				.expectBody() // Проверяем тело ответа.      указание "[]" проверяет пустая ли база данных
+				// Ожидаем, что тело ответа будет соответствовать указанному JSON.
+				.json("""  
 [{"id":-2,"firstName":"Petr","lastName":"Petrov","phone":"+79111111111","birthday":"1990-01-01"},
 {"id":-1,"firstName":"Aleksey","lastName":"Alekseev","phone":"+79000000000","birthday":"1980-01-01"}]
 """);
 	}
-	@Test
+	@Test // Тест для проверки метода сохранения новой записи в адресной книге.
 	void testSave() {
+		// Создаем новый объект AddressBook для сохранения.
 		AddressBook addressBook = new AddressBook(null, "Ivan", "Ivanov", "+799999999999", LocalDate.parse("2000-01-01"));
-
+		// Получаем текущее количество записей в базе данных.
 		long sizeBefore = repository.findAll().count().block();  // block используется чтобы превратить реактивный метод в обычный синхронный
 
-		webTestClient.post()
+		webTestClient.post() // Выполняем POST-запрос к эндпоинту /api/v1/addresbook для сохранения новой записи.
 				.uri("/api/v1/addresbook")
-				.bodyValue(addressBook)
-				.header("Content-Type", "application/json")
-				.exchange()
-				.expectStatus().isOk()
-				.expectBody()
+				.bodyValue(addressBook) // Передаем объект AddressBook в теле запроса.
+				.header("Content-Type", "application/json")// Указываем тип содержимого.
+				.exchange()// Отправляем запрос.
+				.expectStatus().isOk()// Ожидаем статус ответа 200 OK.
+				.expectBody()// Проверяем тело ответа.
+				// Ожидаем, что тело ответа будет соответствовать указанному JSON.
 				.json("""
                  {
                      "firstName": "Ivan",
@@ -61,10 +64,10 @@ class AddressBookApplicationTests {
                       "birthday": "2000-01-01"
                                               }
                                                   """);
-
+        // Получаем количество записей в базе данных после сохранения.
 		long sizeAfter = repository.findAll().count().block();
-		assertEquals(sizeBefore+1, sizeAfter);
-		// можно добавить assertTrue(sizeBefore < sizeAfter); для проверки если вызывать несколько методов добавляющих
+		assertEquals(sizeBefore+1, sizeAfter); // Проверяем, что количество записей увеличилось на 1.
+		// можно добавить assertTrue(sizeBefore < sizeAfter); // Альтернативная проверка: убеждаемся, что количество записей увеличилось.для проверки если вызывать несколько методов добавляющих
 		// данные в БД и тогда может не выполнится услове sizeBefore+1, sizeAfter так как не смог выполнится один из этих методов
 	}
 }
